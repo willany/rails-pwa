fd4bcb46013b2eb8792ef11d5c8a354d1eb5e8a0
Willany Silva <willanysilva@MacBook-Pro-de-Willany.local>
2018-10-13 21:41:56 -0300

    Create Pages and add Home

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+//= require materialize
